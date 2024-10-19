69324d0f8629b84201fd66eecd560f2925d599a3
Sean O'Leary <sean@o-leary.co.nz>
2024-10-19 18:48:38 +1300

    Update startup.sh
    Start wyoming script

--- startup.sh
+++ startup.sh
@@ -7,4 +7,8 @@ echo "First startup may download 2GB of speech models. Please wait."
 bash download_voices_tts-1.sh
 bash download_voices_tts-1-hd.sh $PRELOAD_MODEL
 
+cd wyoming-piper
+script/run --piper '/usr/share/piper/piper' --voice en_US-lessac-medium --uri 'tcp://0.0.0.0:10200' --data-dir /data --download-dir /data &
+cd ..
+
 python speech.py ${PRELOAD_MODEL:+--preload $PRELOAD_MODEL} $EXTRA_ARGS $@
